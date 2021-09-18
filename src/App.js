ffe8952e20827f564a7aa96090a2ef5c1896ba55
Zohar Nyego <zoharngo@gmail.com>
2021-09-18 22:37:34 +0300

    cleaning

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState, useCallback, useRef } from 'react';
+import EventTimeLine from './componets/event-time-line/EventTimeLine';
+import { Container } from '@mui/material';
 
 function App() {
+  const [list, setList] = useState([]);
+  const [page, setPage] = useState(1);
+  const totaltemCount = useRef(0);
+
+  const fetchItems = useCallback(async () => {
+    const res = await fetch(`http://localhost:3001/events?_page=${page}&_limit=15`);
+    totaltemCount.current = res.headers.get('X-Total-Count');
+    const postEvents = await res.json();
+    setList((prevEvents) => [...prevEvents, ...postEvents]);
+  }, [page]);
+
+  useEffect(() => {
+    fetchItems();
+  }, [fetchItems]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container sx={{ position: 'relative', top: '90px', fontSizeAdjust: 'from-font' }} component='section'  maxWidth="sm">
+      {list.length > 0 && (
+        <EventTimeLine
+          height={450}
+          list={list}
+          loadMore={() => {
+            setPage((prevPage) => prevPage + 1);
+          }}
+          hasMore={list.length < totaltemCount.current}
+        />
+      )}
+    </Container>
   );
 }
 
---
+++ src/componets/event-time-line/EventTimeLine.js
@@ -0,0 +1,137 @@
+import { useEffect, useRef, useState } from 'react';
+import { List, AutoSizer } from 'react-virtualized';
+import styles from './EventTimeLine.css';
+import Box from '@mui/material/Box';
+import { CircularProgress, IconButton, Skeleton } from '@mui/material';
+import { ArrowDownward, ArrowUpward } from '@mui/icons-material';
+
+const COLOR_MAP = Object.freeze({
+  GET: 'red',
+  POST: 'AntiqueWhite',
+  DELETE: 'Aqua',
+  PUT: 'Aquamarine',
+  PATCH: 'Azure',
+  HEAD: 'Beige',
+  TRACE: 'Bisque',
+  CONNECT: 'Black',
+  AUTH: 'Green',
+  OPTIONS: 'BlueViolet',
+});
+
+function EventTimeLine({ list, loadMore, hasMore, height }) {
+  const [scrollToIndex, setScrollToIndex] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const [selectedRowIndex, setSelectedRowIndex] = useState(-1);
+  const cacheStopIndex = useRef(0);
+
+  useEffect(() => {
+    if (cacheStopIndex.current === 0) {
+      return;
+    }
+    setIsLoading(() => {
+      setScrollToIndex(cacheStopIndex.current);
+      return false;
+    });
+  }, [list.length]);
+
+  const _noRowsRenderer = () => {
+    return <div className={styles.noRows}>No rows</div>;
+  };
+  const _rowRenderer = ({ index, style, isScrolling, key }) => {
+    return !isScrolling ? (
+      <Box
+        component='div'
+        className={`row ${index === selectedRowIndex ? 'highlighted' : ''}`}
+        key={key}
+        style={style}
+        onClick={(e) => {
+          e.stopPropagation();
+          setSelectedRowIndex(index);
+          const pos = list.length - 1 - index;
+          if (pos < 5) {
+            setScrollToIndex(list.length - 1);
+          } else setScrollToIndex(index + 2);
+        }}
+      >
+        <Box component='div' className='content'>
+          <Box component='span'>{new Date(list[index].timestamp).toLocaleTimeString()}</Box>
+        </Box>
+        <Box component='div' className='content method'>
+          <Box
+            component='span'
+            style={{
+              backgroundColor: COLOR_MAP[list[index].method],
+            }}
+            dangerouslySetInnerHTML={{ __html: `${list[index].method}` }}
+          />
+        </Box>
+        <Box component='div' className='content'>
+          <Box component='span' dangerouslySetInnerHTML={{ __html: list[index].endpoint_path }} />
+        </Box>
+      </Box>
+    ) : (
+      <Box component='div' key={key} style={style} className='row'>
+        <Skeleton sx={{ width: '80%', height: height / 5 }} animation='pulse' />
+      </Box>
+    );
+  };
+
+  const _onRowsRendered = ({ stopIndex }) => {
+    if (hasMore && stopIndex === list.length - 1) {
+      setIsLoading(true);
+      loadMore();
+      cacheStopIndex.current = stopIndex;
+    }
+  };
+  const _onScroll = (direction) => {
+    const scrollFrom = selectedRowIndex ? selectedRowIndex : 0;
+    console.log('direction', direction);
+    console.log('scrollFrom', scrollFrom);
+
+    if (direction === 'down') {
+      setSelectedRowIndex(scrollFrom + 1);
+    } else {
+      setSelectedRowIndex(scrollFrom - 1);
+    }
+  };
+  const EventList = () => {
+    return (
+      <Box component='div' sx={{ textAlign: 'center' }}>
+        <Box component='h1'>Events Timeline</Box>
+        <Box component='div'>
+          {!isLoading ? (
+            <AutoSizer disableHeight>
+              {({ width }) => (
+                <List
+                  className='list'
+                  height={height}
+                  noRowsRenderer={_noRowsRenderer}
+                  rowCount={list.length}
+                  rowHeight={height / 5}
+                  rowRenderer={_rowRenderer}
+                  width={width}
+                  onRowsRendered={_onRowsRendered}
+                  scrollToIndex={scrollToIndex}
+                />
+              )}
+            </AutoSizer>
+          ) : (
+            <CircularProgress sx={{ position: 'relative', top: '150px' }} />
+          )}
+        </Box>
+        <Box component='div'>
+          <IconButton color='secondary' aria-label='up' onClick={() => _onScroll('up')}>
+            <ArrowUpward />
+          </IconButton>
+          <IconButton color='primary' aria-label='down' onClick={() => _onScroll('down')}>
+            <ArrowDownward />
+          </IconButton>
+        </Box>
+      </Box>
+    );
+  };
+
+  return <EventList />;
+}
+
+export default EventTimeLine;
